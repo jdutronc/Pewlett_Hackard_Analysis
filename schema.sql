7da9c827fe66870e1a4387203cf000df6b07f802
jdutronc <108039827+jdutronc@users.noreply.github.com>
2022-08-13 12:33:01 -0400

    Rebuilt PH-EmployeeDB

--- schema.sql
+++ schema.sql
@@ -7,23 +7,34 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-     emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE salaries (
@@ -35,21 +46,14 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_employee (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE title (
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR(40) NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
 );
+
+SELECT * FROM departments;
